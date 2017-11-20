0889106b1ae2d3b710bbc3d1ea3bbf37611ed90f
merdan0596 <merdan.bashimov96@mail.ru>
2017-11-20 03:53:36 +0300

    Первый класс

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[]args){
+    //Hello world коентарий
+    System.out.println("Hello World ");
+    System.out.println("2+2="+(2+2));
+    System.out.println("СТРОКА 1\n\tСтрока 2\n\t Строка3\n\t ");
+}
+}
+
