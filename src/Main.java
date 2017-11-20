59b57a4ab3b23664114576b5adb289cbb52ada7e
merdan0596 <merdan.bashimov96@mail.ru>
2017-11-20 04:22:10 +0300

    ИЗМЕНИЛИ НАШ КЛАСС

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public static void main(String[]args){
     System.out.println("Hello World ");
     System.out.println("2+2="+(2+2));
     System.out.println("СТРОКА 1\n\tСтрока 2\n\t Строка3\n\t ");
+    System.out.println("кОНЕЦ");
 }
 }
 
